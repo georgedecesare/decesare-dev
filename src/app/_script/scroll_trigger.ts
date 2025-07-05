a0976f5e5eaef0dbc6297730db444f9d419e8873
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-07-05 17:19:21 +0100

    feat: timeline

--- src/app/_script/scroll_trigger.ts
+++ src/app/_script/scroll_trigger.ts
@@ -21,42 +21,38 @@ export default function scrollTrigger() {
 
 function moveName() {
   const nameLogo = document.getElementById('name-logo')!;
-  const originalParent = nameLogo.parentNode!;
   const newParent = document.getElementById('logo')!;
-  const navlinks = document.querySelectorAll('#navlink');
-  const footer = document.getElementById('footer');
+  const navlinks = document.querySelectorAll('#navlink')!;
+  const footer = document.getElementById('footer')!;
+  const flipElements = [nameLogo, ...navlinks, footer];
+
+  const tl = gsap.timeline();
+  tl.to(footer, {
+    duration: 0.1,
+    autoAlpha: 0,
+    y: 50,
+    ease: 'power1.inOut',
+  });
+
+  const startState = Flip.getState(flipElements, {
+    props: 'fontSize, lineHeight',
+  });
+
+  newParent.appendChild(nameLogo);
+
+  const flipAnimation = Flip.from(startState, {
+    duration: 1,
+    ease: 'power1.inOut',
+    absoluteOnLeave: true,
+  });
+
+  tl.add(flipAnimation, '<');
 
   ScrollTrigger.create({
     trigger: 'body',
     start: 'top top',
-    end: '+=300',
-    onEnter: () => {
-      // Only move the name when scrolling actually starts
-      const state = Flip.getState([nameLogo, ...navlinks, footer], {
-        props: 'fontSize, lineHeight',
-      });
-
-      newParent.appendChild(nameLogo);
-
-      Flip.from(state, {
-        duration: 0.5,
-        ease: 'power1.inOut',
-        absolute: true,
-      });
-    },
-    onLeaveBack: () => {
-      // Move the name back to its original position when scrolling back to top
-      const state = Flip.getState([nameLogo, ...navlinks, footer], {
-        props: 'fontSize, lineHeight',
-      });
-
-      originalParent.appendChild(nameLogo);
-
-      Flip.from(state, {
-        duration: 0.5,
-        ease: 'power1.inOut',
-        absolute: true,
-      });
-    },
+    end: '+=400',
+    scrub: true,
+    animation: tl, // Tell ScrollTrigger to control our paused animation
   });
 }
