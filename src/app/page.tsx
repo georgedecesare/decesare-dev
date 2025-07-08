01a27a91dfeaf856c125cd52af54132a43483a8b
George Decesare <gdecesare@outlook.com>
2025-07-08 15:13:40 +0100

    fix: Better styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,46 @@ gsap.registerPlugin(SplitText);
 gsap.registerPlugin(TextPlugin);
 
 export default function Home() {
+  const gradient = "bg-gradient-to-r bg-clip-text text-transparent from-blue-400 via-purple-400 to-pink-400";
   useGSAP(() => {
     let split = SplitText.create(".animate-name", {
-      type: "chars"
+      type: "chars",
     });
     const center = Math.floor(split.chars.length / 2);
     const nameElem = document.querySelector(".animate-name") as HTMLElement;
     const centerOffset = nameElem ? nameElem.offsetWidth / 2 : 0;
 
-    const tl = gsap.timeline();
+    const bgSize = nameElem.parentElement!.offsetWidth;
+    const langle = document.querySelector(".animate-langle") as HTMLElement;
+    const rangle = document.querySelector(".animate-rangle") as HTMLElement;
 
+    let offset = 0;
+    const nameWidth = nameElem.offsetWidth;
+
+    langle.style.backgroundSize = bgSize + 'px 100%';
+    langle.style.backgroundPosition = offset + 'px 0%';
+    offset += langle.offsetWidth;
+
+    split.chars.forEach((char, i) => {
+      const elem = char as HTMLElement;
+      elem.className = `${gradient} pb-2`;
+      elem.style.backgroundSize = bgSize + 'px 100%'
+      elem.style.backgroundPosition = -offset + 'px 0%'
+      offset += elem.offsetWidth;
+    });
+    rangle.style.backgroundSize = bgSize + 'px 100%';
+    rangle.style.backgroundPosition = -offset + 'px 0%';
+
+    const tl = gsap.timeline();
+    tl.from(".animate-popup", {
+      delay: 1,
+      duration: 0.2,
+      scale: 0,
+      ease: "back.out",
+    }
+    );
     tl.from(split.chars, {
-      delay: 2,
+      delay: 0.5,
       stagger: {
         each: 0.05,
         from: "center"
@@ -53,13 +81,15 @@ export default function Home() {
   });
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-6xl font-bold font-mono flex flex-row items-center justify-center">
-        <span
-          className="animate-langle"
-        >&lt;</span>
-        <span className="animate-name">George Decesare</span>
-        <span className="animate-rangle">/&gt;</span>
-      </h1>
+      <div className="animate-popup">
+        <h1 className="text-6xl font-bold font-mono flex flex-row items-center justify-center">
+          <div
+            className={`animate-langle ${gradient} pb-2`}
+          >&lt;</div>
+          <div className="animate-name">George Decesare</div>
+          <div className={`animate-rangle ${gradient} pb-2`}>/&gt;</div>
+        </h1>
+      </div>
     </main>
   );
 }
