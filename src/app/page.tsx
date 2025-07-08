16cd3e188a0eed74e764b80efd04da86bb587e4e
George Decesare <gdecesare@outlook.com>
2025-07-08 15:13:40 +0100

    refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
 import { TextPlugin } from "gsap/TextPlugin";
+import "material-symbols"
 
 gsap.registerPlugin(SplitText);
 gsap.registerPlugin(TextPlugin);
 
 export default function Home() {
-  const gradient = "bg-gradient-to-r bg-clip-text text-transparent from-blue-400 via-purple-400 to-pink-400";
+  const initialDelay = 0.2;
+
+  const gradient = "bg-gradient-to-r bg-clip-text text-transparent from-blue-600 via-purple-600 to-pink-600";
   useGSAP(() => {
     let split = SplitText.create(".animate-name", {
       type: "chars",
@@ -23,7 +26,6 @@ export default function Home() {
     const rangle = document.querySelector(".animate-rangle") as HTMLElement;
 
     let offset = 0;
-    const nameWidth = nameElem.offsetWidth;
 
     langle.style.backgroundSize = bgSize + 'px 100%';
     langle.style.backgroundPosition = offset + 'px 0%';
@@ -39,21 +41,25 @@ export default function Home() {
     rangle.style.backgroundSize = bgSize + 'px 100%';
     rangle.style.backgroundPosition = -offset + 'px 0%';
 
+    gsap.set(".animate-popup", {
+      autoAlpha: 1,
+    });
+
     const tl = gsap.timeline();
     tl.from(".animate-popup", {
-      delay: 1,
+      delay: initialDelay,
       duration: 0.2,
       scale: 0,
       ease: "back.out",
     }
     );
     tl.from(split.chars, {
-      delay: 0.5,
+      delay: 0.1,
       stagger: {
-        each: 0.05,
+        each: 0.03,
         from: "center"
       },
-      duration: 1,
+      duration: 0.3,
       opacity: 0,
       y: 0,
       scale: 0.5,
@@ -63,7 +69,7 @@ export default function Home() {
 
     tl.from(".animate-langle", {
       delay: 0,
-      duration: 1.3,
+      duration: 0.8,
       y: 0,
       x: centerOffset,
       ease: "back.out(3.5)",
@@ -72,16 +78,52 @@ export default function Home() {
 
     tl.from(".animate-rangle", {
       delay: 0,
-      duration: 1.3,
+      duration: 0.8,
       y: 0,
       x: -centerOffset,
       ease: "back.out(3.5)",
     }, "<"
     );
+
+    const footer = document.querySelector(".footer-typewriter") as HTMLElement;
+
+    const typewriter = SplitText.create(footer, {
+      type: "chars"
+    });
+    gsap.set(typewriter.chars, {
+      autoAlpha: 0,
+    });
+    gsap.set(footer, {
+      autoAlpha: 1,
+    });
+
+    typewriter.chars.forEach((char) => {
+      tl.to(char, {
+        delay: Math.random() / 20 + 0.02,
+        autoAlpha: 1,
+        animation: "text",
+        duration: 0,
+      });
+    });
+
+    const moreIcon = document.querySelector(".more-icon") as HTMLElement;
+    tl.to(moreIcon, {
+      delay: 0.2,
+      duration: 1,
+      autoAlpha: 1,
+      ease: "power1.out",
+    }
+    );
+    tl.from(moreIcon, {
+      delay: 0,
+      duration: 1,
+      y: -10,
+      ease: "bounce.out",
+    }, "<");
   });
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="animate-popup">
+      <div className="animate-popup opacity-0">
         <h1 className="text-6xl font-bold font-mono flex flex-row items-center justify-center">
           <div
             className={`animate-langle ${gradient} pb-2`}
@@ -89,6 +131,22 @@ export default function Home() {
           <div className="animate-name">George Decesare</div>
           <div className={`animate-rangle ${gradient} pb-2`}>/&gt;</div>
         </h1>
+      </div>
+      <footer className="mt-4 text-center font-terminal text-3xl font-bold footer-typewriter opacity-0">
+        Computing student at Imperial College London.
+      </footer>
+      <div className="mt-4 flex flex-col items-center justify-center">
+        <span
+          className="material-symbols-rounded select-none text-gray-700 opacity-0 more-icon"
+          style={{
+            fontSize: "3rem",
+            fontFamily: "'Material Symbols Rounded'",
+            fontWeight: 400,
+            fontVariationSettings: "'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 48"
+          }}
+        >
+          expand_more
+        </span>
       </div>
     </main>
   );
