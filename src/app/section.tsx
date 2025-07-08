0069c28c946287db1e09baf1707b307e4525b779
George Decesare <gdecesare@outlook.com>
2025-07-08 15:13:42 +0100

    fix: Smart wrap

--- src/app/section.tsx
+++ src/app/section.tsx
@@ -9,15 +9,23 @@ export default function Section({
   title,
 }: React.PropsWithChildren<SectionProps>) {
   return (
-    <section className="px-10 flex flex-row w-full xl:max-w-[80%] invisible">
+    <section
+      className="px-10 flex flex-row w-full xl:max-w-[90%] 2xl:max-w-[70%]
+        invisible"
+    >
       <div className="h-full py-1 pb-20">
         <LeftBraceIcon />
       </div>
-      <div className="flex flex-col font-terminal text-8xl justify-center">
-        <h1 className="text-end pb-20 whitespace-normal">{title}</h1>
+      <div
+        className="flex flex-col font-terminal text-6xl justify-center ml-[-20]
+          lg:text-8xl"
+      >
+        <h1 className="text-end pb-20">{title}</h1>
       </div>
-      <div className="flex items-center flex-2/3">
-        <p className="pl-10 font-mono text-lg w-full pt-20">{children}</p>
+      <div className="flex items-center flex-2/3 3xl:flex-1/3">
+        <p className="pl-10 font-mono text-sm lg:text-md w-full pt-20">
+          {children}
+        </p>
       </div>
       <div className="h-full py-1 pt-20">
         <RightBraceIcon />
